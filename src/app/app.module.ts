1554ca70df54a6a3e6b0cfaae546137d48b1e692
Tristan Andrei <tristanandrei.alipio@letran.edu.ph>
2024-02-27 12:02:33 +0800

    I love you

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
+import { ContactInformationComponent } from './contact-information/contact-information.component';
+import { EducationalBackgroundComponent } from './educational-background/educational-background.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInformationComponent,
+    ContactInformationComponent,
+    EducationalBackgroundComponent,
+    WorkExperienceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/personal-information/personal-information.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-information',
+  templateUrl: './personal-information.component.html',
+  styleUrl: '../../../node_modules/bootstrap/dist/css/bootstrap.css'
+})
+export class PersonalInformationComponent {
+
+}
